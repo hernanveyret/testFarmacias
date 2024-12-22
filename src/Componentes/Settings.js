76f27951c0510ad5f94d38a22a28428fb1442582
hernanveyret <hernanveyret@hotmail.com>
2024-12-22 14:54:13 -0300

    shared app function

--- src/Componentes/Settings.js
+++ src/Componentes/Settings.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './settings.css';
 const Settings = ({settingOptions,almanacType,handleChangeAlmanacType,setModoNocturno,modoNocturno,ubication,setUbication}) => {
+
+  const sharedApp = () => {
+    navigator.clipboard.writeText('https://test-farmacias.vercel.app/')
+    .then(() => {
+      console.log('se copio')
+    })
+    .catch(()=> {
+      console.log('error')
+    })
+  }
+
   return (
     <div className="containerSettings" id="panelSetting">
       <button className="btn-cerrar-settings" onClick={settingOptions} title="Cerrar Menu">X</button>
@@ -39,13 +50,18 @@ const Settings = ({settingOptions,almanacType,handleChangeAlmanacType,setModoNoc
             </button>
           </div>
           <div>
-            <button onClick={() => {setUbication(prevMode => !prevMode); settingOptions()}} title="Ubicacion">        
+            <button onClick={() => {setUbication(prevMode => !prevMode); settingOptions()}} title="Distancia">        
             {
               ubication ?
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#75FB4C"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg>
               :
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg>
             }
+            </button>
+          </div>
+          <div>
+            <button onClick={() => { sharedApp(); settingOptions()}} title="Copiar url al portapapeles">        
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm480-280q17 0 28.5-11.5T720-760q0-17-11.5-28.5T680-800q-17 0-28.5 11.5T640-760q0 17 11.5 28.5T680-720Zm0 520ZM200-480Zm480-280Z"/></svg>
             </button>
           </div>
         </div>
